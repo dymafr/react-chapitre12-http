a73f6240036f9c411b2a61dca74cbae4b6fb4192
Erwan Vallée <erwanp.vallee@gmail.com>
2021-01-15 16:45:37 +0100

    update

--- src/Adduser.js
+++ src/Adduser.js
@@ -2,41 +2,43 @@ import React, { Component } from 'react';
 import { Formik, Field } from 'formik';
 import * as axios from 'axios';
 
-export default class extends Component {
-    
+const AddUser = class extends Component {
   getInitialValues = () => {
-    return this.props.user ? { ...this.props.user } : { name: '', username: '', email: '' } 
-  }
+    return this.props.user
+      ? { ...this.props.user }
+      : { name: '', username: '', email: '' };
+  };
 
-  submit = (values, actions) => { 
+  submit = (values, actions) => {
     if (!this.props.user) {
-      axios.post('/users', values).then( response => {
+      axios.post('/users', values).then((response) => {
         console.log(response);
-      })
+      });
     } else {
-      axios.put(`/users/${ values.id }`, values).then( response => {
+      axios.put(`/users/${values.id}`, values).then((response) => {
         console.log(response);
-      })
+      });
     }
-   
-  }
+  };
 
   render() {
     return (
-      <Formik 
-        initialValues={ this.getInitialValues() }
-        onSubmit={ this.submit }
-        enableReinitialize={ true }
+      <Formik
+        initialValues={this.getInitialValues()}
+        onSubmit={this.submit}
+        enableReinitialize={true}
       >
-        { ({ handleSubmit}) => (
-          <form onSubmit={ handleSubmit } className="d-flex flex-column">
+        {({ handleSubmit }) => (
+          <form onSubmit={handleSubmit} className="d-flex flex-column">
             <Field name="name" placeholder="name" />
             <Field name="username" placeholder="username" />
             <Field name="email" placeholder="email" />
             <button type="submit">save</button>
           </form>
-        ) }
+        )}
       </Formik>
-    )
+    );
   }
-}
+};
+
+export default AddUser;
--- src/Listuser.js
+++ src/Listuser.js
@@ -1,23 +1,39 @@
 import React from 'react';
 
-export default (props) => {
+const listUser = (props) => {
   const users = props.users;
   return (
     <div className="w-100 d-flex flex-row flex-wrap justify-content-center my-3">
-      { users && users.length ? (
-        users.map( (u, index) => (
-          <div key={ u.id } onClick={ () => { props.selectUser(index) } } className="card m-2" style={ { width: '200px'} }>
-            <div className="card-header">{ u.name }</div>
+      {users && users.length ? (
+        users.map((u, index) => (
+          <div
+            key={u.id}
+            onClick={() => {
+              props.selectUser(index);
+            }}
+            className="card m-2"
+            style={{ width: '200px' }}
+          >
+            <div className="card-header">{u.name}</div>
             <div className="card-body">
               <ul className="list-group">
-                <li className="list-group-item" >{ u.username }</li>
-                <li className="list-group-item" >{ u.email }</li>
+                <li className="list-group-item">{u.username}</li>
+                <li className="list-group-item">{u.email}</li>
               </ul>
-              <button className="btn btn-small btn-danger" onClick={ () => props.deleteUser(u.id) } >delete</button>
+              <button
+                className="btn btn-small btn-danger"
+                onClick={() => props.deleteUser(u.id)}
+              >
+                delete
+              </button>
             </div>
           </div>
         ))
-      ) : (<h1 className="text-center"> No users ... </h1>) }
+      ) : (
+        <h1 className="text-center"> No users ... </h1>
+      )}
     </div>
-  )
-}
+  );
+};
+
+export default listUser;
